0598925d7489f58c0d73ef61d5283c66f4af8ba7
Aliya Merali <merali4@gmail.com>
2021-04-20 14:26:05 -0600

    Add wokring total_inventory method

--- lib/event.rb
+++ lib/event.rb
@@ -22,4 +22,27 @@ class Event
     end
   end
 
+  def total_inventory
+    total_inventory = {}
+    @food_trucks.each do |food_truck|
+      food_truck.inventory.each do |item, count|
+        if total_inventory[item] != nil
+          total_inventory[item][:quantity] += count
+          total_inventory[item][:food_trucks] << food_truck
+        else
+          total_inventory[item] = {quantity: count, food_trucks: [food_truck]}
+        end
+      end
+    end
+    total_inventory
+  end
+
+  #ITERATION 3:
+  ##sorted_item_list that returns a list of names of all items the FoodTrucks
+  # have in stock, sorted alphabetically. This list should not include any
+  # duplicate items.
+
+  ##overstocked_items. An item is overstocked if it is sold by more than 1 food
+  # truck AND the total quantity is greater than 50.
+
 end
