7734137e480277a4125a1208f0835a765be0db78
Aliya Merali <merali4@gmail.com>
2021-04-20 14:13:13 -0600

    Add test for #total_inventory

--- spec/event_spec.rb
+++ spec/event_spec.rb
@@ -18,12 +18,11 @@ RSpec.describe Event do
 
   describe 'food truck management' do
     event = Event.new("South Pearl Street Farmers Market")
-    food_truck1 = FoodTruck.new("Rocky Mountain Pies")
-    food_truck1 = FoodTruck.new("Rocky Mountain Pies")
     item1 = Item.new({name: 'Peach Pie (Slice)', price: "$3.75"})
     item2 = Item.new({name: 'Apple Pie (Slice)', price: '$2.50'})
     item3 = Item.new({name: "Peach-Raspberry Nice Cream", price: "$5.30"})
     item4 = Item.new({name: "Banana Nice Cream", price: "$4.25"})
+    food_truck1 = FoodTruck.new("Rocky Mountain Pies")
     food_truck1.stock(item1, 35)
     food_truck1.stock(item2, 7)
     food_truck2 = FoodTruck.new("Ba-Nom-a-Nom")
@@ -31,6 +30,7 @@ RSpec.describe Event do
     food_truck2.stock(item3, 25)
     food_truck3 = FoodTruck.new("Palisade Peach Shack")
     food_truck3.stock(item1, 65)
+    food_truck3.stock(item3, 10)
 
 
     it '#add_food_truck adds a food truck to the food_trucks array' do
@@ -52,6 +52,19 @@ RSpec.describe Event do
 
       expect(event.food_trucks_that_sell(item1)).to eq([food_truck1, food_truck3])
       expect(event.food_trucks_that_sell(item4)).to eq([food_truck2])
+    end
+
+    it '#total_inventory returns hash with items as keys, hashes as values' do
+
+      expected = {
+        item1 => {quantity: 100, food_trucks: [food_truck1, food_truck3]},
+        item2 => {quantity: 7, food_trucks: [food_truck1]},
+        item3 => {quantity: 35, food_trucks: [food_truck2, food_truck3]},
+        item4 => {quantity: 50, food_trucks: [food_truck2]},
+      }
+      expect(event.total_inventory.class).to eq(Hash)
+      expect(event.total_inventory.keys[0].class).to eq(Item)
+      expect(event.total_inventory).to eq(expected)
     end
   end
 
