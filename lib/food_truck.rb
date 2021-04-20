a1170ebac015bd3208f573ea0809ed5536598510
Aliya Merali <merali4@gmail.com>
2021-04-20 13:27:04 -0600

    Add passing stock and check_stock methods

--- lib/food_truck.rb
+++ lib/food_truck.rb
@@ -5,4 +5,20 @@ class FoodTruck
     @name = name
     @inventory = {}
   end
+
+  def stock(item, count)
+    if inventory[item] == nil
+      inventory[item] = count
+    else
+      inventory[item] +=  count
+    end
+  end
+
+  def check_stock(item)
+    if inventory.keys.include?(item)
+      inventory[item]
+    else
+      0
+    end
+  end
 end
